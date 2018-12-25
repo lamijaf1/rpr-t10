7043fa32d5cc5bf3410cfe553774b3251f8ac1b4
Lamija <lfazlija1@etf.unsa.ba>
2018-12-25 11:12:03 +0100

    dodavanje metoda

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -5,19 +5,34 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Main extends Application {
-
+    public  String ispisiGradove(){
+        ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
+        String s="";
+        for(Grad grad: gradovi){
+            s += grad.toString();
+        }
+        return s;
+    }
+    public static void glavniGrad(){
+        System.out.println("Unesite naziv drzave: ");
+        Scanner ulaz = new Scanner(System.in);
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        String naziv = ulaz.nextLine();
+        Grad glavniGrad = dao.glavniGrad(naziv);
+        if(glavniGrad == null) System.out.println("Nepostojeca drzava");
+        else System.out.println("Glavni grad drzave " + naziv + " je " + glavniGrad.getNaziv());
+    }
     @Override
     public void start(Stage primaryStage) throws Exception{
         GeografijaDAO model = GeografijaDAO.getInstance();
         //model.napuni();
         ResourceBundle bundle = ResourceBundle.getBundle("Translation");
-        FXMLLoader loader = new FXMLLoader( getClass().getResource(
-                "/biblioteka.fxml" ), bundle);
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader( getClass().getResource("/sample.fxml" ), bundle);
         loader.setController(new GeografijaDAO(model));
         Parent root = loader.load();
         primaryStage.setTitle("Geografija");
