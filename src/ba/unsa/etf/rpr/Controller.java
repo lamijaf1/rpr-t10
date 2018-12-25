e94f9957a480c508eaa2325798d32359bad06d42
Lamija <lfazlija1@etf.unsa.ba>
2018-12-25 18:25:54 +0100

    popravljeno za jezike

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -4,16 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import net.sf.jasperreports.engine.JRException;
 
+import java.util.ResourceBundle;
+
 
 public class Controller {
     public TextField drzavaNaziv;
     public Button stampaj;
     public Button pronadjiGlavniGrad;
     public Button obrisiDrzavu;
-    public Button ispisGradova;
-    public Menu file,help,view,jezik;
+    public Button ispisGradova,pronadjiDrzavu;
+    public Menu file,help,view,Jezik;
+    public  MenuItem bosanski,njemacki,engleski,francuski,close,saveAs;
     public Main Main;
-    public Label statusMsg;
+    public Label statusMsg,drzava;
 
 
     public void obrisiDrzavu(ActionEvent actionEvent) {
@@ -52,10 +55,39 @@ public class Controller {
     }
 
     public void jezik(ActionEvent actionEvent) {
-        String izaberi="";
-        if(((MenuItem) actionEvent.getTarget()).getId()=="bosanski")izaberi="Translation_bs";
-        if(((MenuItem) actionEvent.getTarget()).getId()=="francuski")izaberi="Translation_fr";
-        if(((MenuItem) actionEvent.getTarget()).getId()=="njemacki")izaberi="Translation_de";
-        if(((MenuItem) actionEvent.getTarget()).getId()=="engleski")izaberi="Translation_en_US";
+       String property="";
+       MenuItem m=(MenuItem)actionEvent.getSource();
+       switch(m.getId()) {
+           case "bosanski":
+               property="Translation_bs";
+               break;
+           case "francuski":
+               property="Translation_fr";
+               break;
+           case "njemacki":
+               property="Translation_de";
+               break;
+           case "engleski":
+               property="Translation_en_US";
+               break;
+       }
+        file.setText(ResourceBundle.getBundle(property).getString("File"));
+        help.setText(ResourceBundle.getBundle(property).getString("Help"));
+        view.setText(ResourceBundle.getBundle(property).getString("View"));
+        close.setText(ResourceBundle.getBundle(property).getString("Close"));
+        saveAs.setText(ResourceBundle.getBundle(property).getString("SaveAs"));
+        Jezik.setText(ResourceBundle.getBundle(property).getString("jezik"));
+        obrisiDrzavu.setText(ResourceBundle.getBundle(property).getString("obrisiDrzavu"));
+        ispisGradova.setText(ResourceBundle.getBundle(property).getString("ispisiGradove"));
+        pronadjiDrzavu.setText(ResourceBundle.getBundle(property).getString("pronadiDrzavu"));
+        pronadjiGlavniGrad.setText(ResourceBundle.getBundle(property).getString("pronadiGlavniGrad"));
+        stampaj.setText(ResourceBundle.getBundle(property).getString("stampaj"));
+        statusMsg.setText(ResourceBundle.getBundle(property).getString("statusMsg"));
+        bosanski.setText(ResourceBundle.getBundle(property).getString("bosanski"));
+        engleski.setText(ResourceBundle.getBundle(property).getString("engleski"));
+        francuski.setText(ResourceBundle.getBundle(property).getString("francuski"));
+        njemacki.setText(ResourceBundle.getBundle(property).getString("njemacki"));
+        drzava.setText(ResourceBundle.getBundle(property).getString("drzava"));
+        statusMsg.setText(ResourceBundle.getBundle(property).getString("statusMsg"));
     }
 }
